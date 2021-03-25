7bc76f924aee291ef5188bd3e0d3178c8de05db3
saratiedt <saracristinetiedt@gmail.com>
2021-03-24 21:57:43 -0300

    design tela

--- vue-weather/src/App.vue
+++ vue-weather/src/App.vue
@@ -4,6 +4,17 @@
       <div class="search-box">
         <input type="text" class="search-bar" placeholder="Pesquisar..." />
       </div>
+
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">Blumenau, Brasil</div>
+          <div class="date">Quarta feira 26 maio, 2021</div>
+        </div>
+        <div class="weather-box">
+          <div class="temp">15°c</div>
+          <div class="weather">Chuva</div>
+        </div>
+      </div>
     </main>
   </div>
 </template>
@@ -74,5 +85,48 @@ main {
   box-shadow: 0px 0px 8pc rgba(0, 0, 0, 0.25);
   background-color: rgba(255, 255, 255, 0.75);
   border-radius: 16px 0px 16px 0px;
+}
+
+.location-box .location {
+  color: white;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+
+.location-box .date {
+  color: white;
+  font-size: 20;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+
+.weather-box {
+  text-align: center;
+}
+
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: white;
+  font-size: 102px;
+  font-weight: 900;
+
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .weather {
+  color: white;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
 }
 </style>
